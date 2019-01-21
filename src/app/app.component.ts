55e2a28df42afe84ba9dc035ab3e3ba48f8b36e0
dunyung1 <dshege4@gmail.com>
2019-01-21 09:59:09 +0300

    back to the basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import {GitsearchService } from './gitsearches/gitsearch.service'
-import {HTTP_PROVIDERS} from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [HTTP_PROVIDERS, GitsearchService]
 })
 export class AppComponent {
 
--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,17 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import {GitsearchService} from '../gitsearches/gitsearch.service'
-
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.css']
 })
-export class InfoComponent implements OnInit {
+export class InfoComponent implements OnInit{
 
-  constructor(private gitsearch:GitsearchService) { }
 
   ngOnInit() {
   }
-
 }
