6b71a7c27788423dd18b92bb7adb126316af25da
dunyung1 <dshege4@gmail.com>
2019-01-22 15:49:18 +0300

    add profile class file

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../../services/profile.service'
-
+import { Profile } from '../profile'
 
 @Component({
   selector: 'app-profile',
@@ -8,7 +8,7 @@ import { ProfileService } from '../../services/profile.service'
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  profile: any[];
+  profile: Profile;
   repos: any[];
   username: string;
 
